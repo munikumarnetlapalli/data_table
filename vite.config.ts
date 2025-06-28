6f45bbf9a9b07cf8139db43f0c42a675943bd438
munikumarnetlapalli <munikumarnetlapalli@gmail>
2025-06-28 17:17:32 +0530

    Set base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/data-table/', // 👈 required for GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
