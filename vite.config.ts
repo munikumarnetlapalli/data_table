03b3cd70dc2353fa1156a710331017576f951a9e
munikumarnetlapalli <munikumarnetlapalli@gmail>
2025-06-29 10:29:11 +0530

    your message

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/data-table/', // 👈 required for GitHub Pages
+ base:process.env.VITE_BASE_PATH || "/data-table", 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  
 });
 
