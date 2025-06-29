57557ec63c317f6d2d5ab05c935ed933f9121c7c
munikumarnetlapalli <munikumarnetlapalli@gmail>
2025-06-29 10:35:20 +0530

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
- base:process.env.VITE_BASE_PATH || "/data-table", 
+ base: process.env.VITE_BASE_PATH || "/data_table", 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
